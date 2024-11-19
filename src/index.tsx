1dd41967887bceebd84dbf6e912a3bedef9e27c7
c9kisben <c9kisben@vasvari.org>
2024-11-19 11:13:52 +0100

    Gang

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Szamolo from './Pages/Szamolo';
+import Atvalto from './Pages/Atvalto';
+import Hiba from './Pages/Hiba';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+    <Routes>
+      <Route path='/szamolo' element={<Szamolo />}/>
+      <Route path='/' element = {<App/>}/>
+      <Route path='/atvalto' element = {<Atvalto/>}/>
+      <Route path='*' element = {<Hiba/>}/>
+    </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
